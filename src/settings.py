5de27e9b0eabc507467902e5babf001ac789ce29
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-10-30 17:05:53 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -54,6 +54,7 @@ CLASS2COLOR: Optional[Dict[str, List[str]]] = {"sheep": [255, 0, 0]}
 PAPER: Optional[str] = None
 CITATION_URL: Optional[str] = "https://www.kaggle.com/datasets/andrewmvd/sheep-detection"
 AUTHORS: Optional[List[str]] = ["Andrew Maranhão"]
+AUTHORS_CONTACTS: Optional[List[str]] = ["https://www.kaggle.com/andrewmvd"]
 
 ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
 ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
@@ -101,6 +102,7 @@ def get_settings():
     settings["paper"] = PAPER
     settings["citation_url"] = CITATION_URL
     settings["authors"] = AUTHORS
+    settings["authors_contacts"] = AUTHORS_CONTACTS
     settings["organization_name"] = ORGANIZATION_NAME
     settings["organization_url"] = ORGANIZATION_URL
     settings["slytagsplit"] = SLYTAGSPLIT
